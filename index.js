1a3e3adc2048f2bdadc25b3a6e8e0f67e95e2fcd
raimondas-27 <raimondasvilnius@yahoo.com>
2021-05-26 21:32:12 +0300

    added 3 routes to project setted a hrefs to different html added plain cars.js file

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 const express = require('express');
 const path = require('path');
+
+const app = express();
+
+app.listen(4000,() => console.log("server is runnning"));
+
+const postsPath = path.join(__dirname,'src', 'html', "posts.html");
+const aboutPath = path.join(__dirname,'src', 'html', 'about.html');
+const contactPath = path.join(__dirname,'src', 'html', 'contact.html');
+
+
+app.get("/posts",(request, response) =>{
+   response.sendFile(postsPath)
+})
+
+app.get("/about",(request, response) =>{
+   response.sendFile(aboutPath)
+})
+
+app.get("/contact",(request, response) =>{
+   response.sendFile(contactPath)
+})
